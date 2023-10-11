2ff709d5de6c6dad2174e19f95286c7d900e6a02
winth03 <nutpapop.win@gmail.com>
2023-10-11 16:10:18 +0700

    added requirements.txt and refactor code

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import cv2
 from cvzone.HandTrackingModule import HandDetector
-from websocket import ABNF
 from mywebsocket import WebSocket
-import numpy as np
 from threading import Thread
+from websocket import ABNF
 from queue import Queue
+import numpy as np
+import cv2
 
 def findHand():
     while True:
         try:
             # Get image from queue
-            img = findHandQueue.get()
+            img = imgQueue.get()
 
             # Check if image is None
             if img is None:
@@ -19,17 +19,15 @@ def findHand():
             # Find hand
             hands, frame = detector.findHands(img)
 
-            # if hands:
-            #     hand1 = hands[0]
-            #     fingers = detector.fingersUp(hand1)
-                # myWebSocket.ws.send(fingers, ABNF.OPCODE_BINARY)
-
-            # Flip image
-            # frame = cv2.flip(frame, 1)
+            if hands:
+                hand1 = hands[0]
+                fingers = detector.fingersUp(hand1)
+                fingers = fingers.count(1)
+                fingerSocket.ws.send(fingers, ABNF.OPCODE_BINARY)
 
             # cv2.imshow("Image", frame)
             newImg = cv2.imencode(".jpg", frame)[1]
-            myWebSocket.ws.send(newImg, ABNF.OPCODE_BINARY)
+            imageSocket.ws.send(newImg, ABNF.OPCODE_BINARY)
             if (cv2.waitKey(1) & 0xFF) == ord('q'):
                 break
         except:
@@ -41,19 +39,20 @@ def on_data(ws, data, data_type, continue_flag):
     nparr = np.frombuffer(data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    findHandQueue.put_nowait(img)
+    imgQueue.put_nowait(img)
 
 def on_error(ws, error):
     print(error)
 
-url = "ws://localhost:1880/opencv" # URL of Node-RED server
-myWebSocket = WebSocket(url, on_error=on_error, on_data=on_data)
+url = "ws://localhost:1880" # URL of Node-RED server
+imageSocket = WebSocket(f"{url}/image", on_error=on_error, on_data=on_data)
+fingerSocket = WebSocket(f"{url}/opencv", on_error=on_error, on_data=on_data)
 detector = HandDetector(detectionCon=0.5, maxHands=1)
-findHandQueue = Queue()
+imgQueue = Queue()
 
 if __name__ == "__main__":
     t1 = Thread(target=findHand)
     t1.daemon = True
     t1.start()
-    myWebSocket.run_forever()
+    imageSocket.run_forever()
     cv2.destroyAllWindows()
